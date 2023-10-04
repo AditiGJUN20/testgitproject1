e8ac3e7473a798c1855779a160dff2d3add8a4fc
Aditi_G <aditi.gajipara@gmail.com>
2023-10-04 02:05:47 -0700

    Added action class variable

--- src/main/java/base/PredefinedActions.java
+++ src/main/java/base/PredefinedActions.java
@@ -1,9 +1,11 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class PredefinedActions {
 
 	private static WebDriver driver;
+	private static Actions action;
 	
 }
